0301f0d6992116bfaddaa8fe0b20d569b7001928
Henrique <henriquesanches@zely.pro>
2026-02-06 01:21:21 -0300

    Revert "Merge pull request #5 from Artificiallltda/feat-Navigation_structure"
    This reverts commit a35294c4038f704aabbae1fe23eafe8fdea24dce, reversing
    changes made to 6648d82cfabee1a35bcf53d2116c341aa1393202.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,3 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Dashboard from "./pages/Dashboard";
-import Sobre from "./pages/Sobre";
-
-function Page({ title }) {
-  return (
-    <div className="page">
-      <h2>{title}</h2>
-    </div>
-  );
-}
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-
-          {/* Menu Principal */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/favoritos" element={<Page title="Lista de Favoritos" />} />
-          <Route path="/mensagens" element={<Page title="Mensagens" />} />
-
-          {/* Meus Imóveis */}
-          <Route path="/imoveis" element={<Page title="Lista de Imóveis" />} />
-          <Route path="/meus-favoritos" element={<Page title="Meus Favoritos" />} />
-          <Route path="/personalizar" element={<Page title="Personalizar" />} />
-
-          {/* Institucional */}
-          <Route path="/sobre" element={<Sobre />} />
 import { useEffect, useMemo, useState } from 'react'
 import {
   Button,
@@ -233,10 +200,4 @@ function App() {
   )
 }
 
-          {/* 404 */}
-          <Route path="*" element={<Page title="404 - Não encontrado" />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export default App
